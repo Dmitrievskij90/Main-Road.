5396b56cb10aca97b431c0034afbe58e497f9019
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-04-11 14:41:04 +0600

    add UIButton extension

---
+++ Main Road/UIButton+UIUtilites.swift
@@ -0,0 +1,23 @@
+//
+//  UIButton+UIUtilites.swift
+//  Main Road
+//
+//  Created by Konstantin Dmitrievskiy on 11.04.2021.
+//
+
+import UIKit
+
+extension UIButton {
+    func apllyButtonCornerRadius(_ radius: CGFloat) {
+        clipsToBounds = true
+        layer.cornerRadius = radius
+    }
+
+    func setButtonShadowWithColor(color: CGColor?, opacity: Float?, offset: CGSize?, radius: CGFloat?, masksToBounds: Bool) {
+        layer.shadowColor = color ?? UIColor.black.cgColor
+        layer.shadowOffset = offset ?? CGSize.zero
+        layer.shadowOpacity = opacity ?? 1.0
+        layer.shadowRadius = radius ?? 1.0
+        layer.masksToBounds = false
+    }
+}
