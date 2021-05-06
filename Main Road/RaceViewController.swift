9c4ef36dca205c44908231759081ae2490e26689
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-06 22:40:58 +0600

    add RaceViewController and RaceViewController.storyboard

---
+++ Main Road/RaceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RaceViewController.swift
+//  Main Road
+//
+//  Created by Konstantin Dmitrievskiy on 06.05.2021.
+//
+
+import UIKit
+
+class RaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
