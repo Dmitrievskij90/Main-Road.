75031ade875178d700027e962e1f481778e77f73
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-07 23:25:22 +0600

    add setupObstacleGesterRecognizer func

--- Main Road/ViewControllers/Welcome Screen/SettingsViewController.swift
+++ Main Road/ViewControllers/Welcome Screen/SettingsViewController.swift
@@ -11,7 +11,7 @@ class SettingsViewController: UIViewController {
     private let defaults = UserDefaults.standard
     private var index = 0
     private var selectedImage = "ic_barrel"
-    private var level:Double = 0.04
+    private var level: Double = 0.04
     private var levelName = ""
     private var cars = [UIImage(named: "ic_yellowCar"), UIImage(named: "ic_silverCar"), UIImage(named: "ic_redCar")]
     private var carName = ["ic_yellowCar", "ic_silverCar", "ic_redCar"]
@@ -29,6 +29,7 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
         setupSwipeGestureRecognizer()
+        setupObstacleGesterRecognizer()
     }
 
     @IBAction private func selectButtonPressed(_ sender: UIButton) {
@@ -57,7 +58,7 @@ class SettingsViewController: UIViewController {
         setupLabel(label: hardLavelLabel, title: "hard")
     }
 
-    private func setupLabel(label: UILabel, title : String) {
+    private func setupLabel(label: UILabel, title: String) {
         label.backgroundColor = .white
         label.text = title
         label.textColor = .black
@@ -102,10 +103,8 @@ class SettingsViewController: UIViewController {
    private func checkIndex() {
         if index >= cars.count {
             index = 0
-
         } else if index <= -1 {
             index = cars.count - 1
-
         }
         upDateUI()
     }
@@ -123,5 +122,66 @@ class SettingsViewController: UIViewController {
             }
         }
     }
-    
+
+    private func setupObstacleGesterRecognizer() {
+        let barrelTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(fitrstimageTapped(tapGestureRecognizer:)))
+        let holeTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(secondimageTapped(tapGestureRecognizer:)))
+        let tyresTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(thirdimageTapped(tapGestureRecognizer:)))
+
+        barrelImageView.isUserInteractionEnabled = true
+        holeImageView.isUserInteractionEnabled = true
+        tyresImageView.isUserInteractionEnabled = true
+
+        barrelImageView.addGestureRecognizer(barrelTapGestureRecognizer)
+        holeImageView.addGestureRecognizer(holeTapGestureRecognizer)
+        tyresImageView.addGestureRecognizer(tyresTapGestureRecognizer)
+    }
+
+    @objc func fitrstimageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        selectedImage = "ic_barrel"
+
+
+//        barrelImageView.backgroundColor = .init(hex: 0xf0e3ca)
+
+        barrelImageView.layer.cornerRadius = 25
+        barrelImageView.clipsToBounds = true
+        barrelImageView.layer.borderColor = UIColor.darkGray.cgColor
+        barrelImageView.layer.borderWidth = 1.5
+
+        resetImageViewSettings(imageView: tyresImageView)
+        resetImageViewSettings(imageView: holeImageView)
+    }
+
+    @objc func secondimageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        selectedImage = "ic_hole"
+
+//        holeImageView.backgroundColor = .init(hex: 0xf0e3ca)
+        holeImageView.layer.cornerRadius = 25
+        holeImageView.clipsToBounds = true
+        holeImageView.layer.borderColor = UIColor.darkGray.cgColor
+        holeImageView.layer.borderWidth = 1.5
+
+        resetImageViewSettings(imageView: barrelImageView)
+        resetImageViewSettings(imageView: tyresImageView)
+    }
+
+    @objc func thirdimageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        selectedImage = "ic_tyres"
+
+//        tyresImageView.backgroundColor = .init(hex: 0xf0e3ca)
+        tyresImageView.layer.cornerRadius = 25
+        tyresImageView.clipsToBounds = true
+        tyresImageView.layer.borderColor = UIColor.darkGray.cgColor
+        tyresImageView.layer.borderWidth = 1.5
+
+        resetImageViewSettings(imageView: barrelImageView)
+        resetImageViewSettings(imageView: holeImageView)
+    }
+
+    private func resetImageViewSettings(imageView: UIImageView) {
+        imageView.backgroundColor = .white
+
+        imageView.layer.borderWidth = 0
+    }
+
 }
