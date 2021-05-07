c6cedf3d5a04bfcdaa08f25be01aabd18965c780
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-07 22:57:43 +0600

    add outlets

--- Main Road/ViewControllers/Welcome Screen/SettingsViewController.swift
+++ Main Road/ViewControllers/Welcome Screen/SettingsViewController.swift
@@ -8,7 +8,22 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var barrelImageView: UIImageView!
+    @IBOutlet weak var holeImageView: UIImageView!
+    @IBOutlet weak var tyresImageView: UIImageView!
+    @IBOutlet weak var easyLevelLabel: UILabel!
+    @IBOutlet weak var mediumLevelLabel: UILabel!
+    @IBOutlet weak var hardLavelLabel: UILabel!
+    @IBOutlet weak var selectButton: UIButton!
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    @IBAction func selectButtonPressed(_ sender: UIButton) {
+    }
+    
 }
