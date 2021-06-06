171857488837036efbf6c56dc29a40f26158d6e1
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-06-06 19:50:34 +0600

    add Constants file

---
+++ Main Road/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  Main Road
+//
+//  Created by Konstantin Dmitrievskiy on 06.06.2021.
+//
+
+import Foundation
+
+enum Constants {
+    static let yellowCar = "ic_yellowCar"
+    static let redCar = "ic_redCar"
+    static let silverCar = "ic_silverCar"
+    static let policeCar = "ic_barrier"
+    static let mainCar = "ic_mainCar"
+    static let hole = "ic_hole"
+    static let barrier = "ic_barrier"
+    static let barrel = "ic_barrel"
+    static let easy = "easy"
+    static let medium = "medium"
+    static let hard = "hard"
+    static let settingsID = "SettingsID"
+    static let recordsID = "RecordsID"
+}
